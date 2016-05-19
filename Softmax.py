6a6235825443a007f16537c00f460347823f3db1
sudhansusingh22 <sudhansusingh22@gmail.com>
2016-05-18 20:53:43 -0400

    softmax function

--- Softmax.py
+++ Softmax.py
@@ -14,6 +14,11 @@ def softmax(x):
 
 print(softmax(scores))
 
+a = 1000000000
+for i in xrange(1000000):
+    a+= 1e-6
+print(a - 1000000000)
+
 # Plot softmax curves
 import matplotlib.pyplot as plt
 x = np.arange(-2.0, 6.0, 0.1)
